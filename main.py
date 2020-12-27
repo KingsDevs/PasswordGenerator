ff0e79e2dc8244c440e960252ebfd10c8bc69ea7
KingsDevs <sunnyckair@gmail.com>
2020-12-28 02:51:51 +0800

    Working with Front-End

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     password = pass_gen.generate_pass(10)
+    
     return render_template("index.html", genpass=password)
 
 if __name__ == "__main__":
